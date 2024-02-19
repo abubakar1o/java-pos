33d3160726a4c7f97d5bb005a0ec59a1c4909a7c
abubakar1o <155629646+abubakar1o@users.noreply.github.com>
2024-02-19 16:56:21 +0500

    Delete micropos-email directory

--- micropos-email/src/main/java/com/micropos/email/publisher/EventPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.micropos.email.publisher;
-
-import com.micropos.email.dto.EmailMetaData;
-
-public interface EventPublisher {
-
-	void proceedEmailEvent(EmailMetaData emailMetaData);
-
-}
